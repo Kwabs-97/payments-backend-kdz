c43ca06f42406f13c1ec00e2ea5dc1b8812997fd
Samuel <khobewayne@gmail.com>
2025-03-25 07:37:11 +0000

    refactored plans to plans.data.js.. db config for mongoose connection configuration

--- models/index.js
+++ models/index.js
@@ -5,60 +5,7 @@ import connect from "../app.js";
 await connect();
 
 const insertData = async () => {
-  const plansData = [
-    {
-      name: "Freemium",
-      price: 0,
-      period: "monthly",
-      status: "A",
-      features: {
-        videos: false,
-        audio: true,
-        download: false,
-        streaming: false,
-        customize: false,
-      },
-    },
-    {
-      name: "Bronze",
-      price: 30,
-      period: "monthly",
-      status: "A",
-      features: {
-        videos: true,
-        audio: true,
-        download: false,
-        streaming: false,
-        customize: false,
-      },
-    },
-    {
-      name: "Silver",
-      price: 50,
-      period: "monthly",
-      status: "A",
-      features: {
-        videos: true,
-        audio: true,
-        download: false,
-        streaming: false,
-        customize: true,
-      },
-    },
-    {
-      name: "Platinum",
-      price: 100,
-      period: "monthly",
-      status: "A",
-      features: {
-        videos: true,
-        audio: true,
-        download: true,
-        streaming: true,
-        customize: true,
-      },
-    },
-  ];
+  
 
   // insert plans
   const insertPlans = async () => {
