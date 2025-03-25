a1eacc6230879e1e9c0bfbd99b39039f709a0e3a
Samuel <khobewayne@gmail.com>
2025-03-25 12:05:30 +0000

    clean databases

--- model/index2.js
+++ model/index2.js
@@ -3,3 +3,9 @@ import { plansData } from "../data/plans.data";
 import { Subscription, Plan } from "../schemas";
 
 // clear database records for plans and subscriptions
+const clearData = async () => {
+  console.log("cleaning database -- start");
+  await Plan.deleteMany({});
+  await Subscription.deleteMany({});
+  console.log("cleaning databse -- end");
+};
