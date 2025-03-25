dfd55e95ff8414a5a1a696f117a48e632b92e897
Samuel <khobewayne@gmail.com>
2025-03-25 03:36:52 +0000

    modell redefined

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-import env from "dotenv";
-env.config();
-import { MongoClient } from "mongodb";
-import { uri } from "./atlas_uri.js";
 
-const client = new MongoClient(uri);
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = "mongodb+srv://thisissamuelyeboah:KhobeWayne@kwabscluster.d7jjk.mongodb.net/?retryWrites=true&w=majority&appName=KwabsCluster";
 
-const connectDB = async () => {
-  try {
-    await client.connect();
-    console.log("successfully connnect to the database");
-  } catch (error) {
-    console.log("error connection to database");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-};
+});
 
-connectDB();
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
--- atlas_uri.js
+++ atlas_uri.js
@@ -1,1 +1,4 @@
-export const uri = `mongodb+srv://thisissamuelyeboah:${process.env.DB_USER_PASSWORD}@kwabscluster.d7jjk.mongodb.net/?retryWrites=true&w=majority&appName=KwabsCluster`
+import env from "dotenv";
+env.config();
+export const uri = `mongodb+srv://thisissamuelyeboah:${process.env.DB_USER_PASSWORD}@kwabscluster.d7jjk.mongodb.net/?retryWrites=true&w=majority&appName=KwabsCluster`;
+
