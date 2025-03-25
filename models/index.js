24a8e38e97bfa4f8b2f0c60b14922e76af644359
Samuel <khobewayne@gmail.com>
2025-03-25 04:20:18 +0000

    insert plans operation setup complete

--- models/index.js
+++ models/index.js
@@ -1,5 +1,63 @@
-import mongoose, { model } from "mongoose";
-import { plansSchema, subscriptionSchema } from "../schemas";
+import { Plans, Susbscription } from "../schemas/model"
+const plansData = [{
+    name: 'Freemium',
+    price: 0,
+    period: 'monthly',
+    status: 'A',
+    features: {
+        videos: false,
+        audio: true,
+        download: false,
+        streaming: false,
+        customize: false
+    }
+},
+{
+    name: 'Bronze', price: 30,
+    period: 'monthly',
+    status: 'A',
+    features: {
+        videos: true,
+        audio: true,
+        download: false,
+        streaming: false,
+        customize: false
+    }
+},
+{
+    name: 'Silver',
+    price: 50,
+    period: 'monthly',
+    status: 'A',
+    features: {
+        videos: true,
+        audio: true,
+        download: false,
+        streaming: false,
+        customize: true
+    }
+},
+{
+    name: 'Platinum',
+    price: 100,
+    period: 'monthly',
+    status: 'A',
+    features: {
+        videos: true,
+        audio: true,
+        download: true,
+        streaming: true,
+        customize: true
+    }
+}
 
-const Susbscription = mongoose.model("Subscription", subscriptionSchema);
-const Plans = mongoose.model("Plans", plansSchema);
+]
+
+// insert plans 
+const insertPlans = async () => {
+    console.log("inserting plans data")
+    await Plans.insertMany(plansData);
+    console.log("plans data inserted -- complete")
+}
+
+insertPlans()
