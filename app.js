59c338ed8a972b1b27336dd5227ccdb771f3df2a
Samuel <khobewayne@gmail.com>
2025-03-25 07:45:36 +0000

    refactored db connection to config

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-import mongoose from "mongoose";
-// connect to mongo with mongoose
-const connect = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://thisissamuelyeboah:KhobeWayne@kwabscluster.d7jjk.mongodb.net/",
-      { dbName: "subs" }
-    );
-    console.log("connection to subs database success from mongoose");
-  } catch (error) {
-    throw error("Error connecting to your database", error);
-  }
-};
+import env from "dotenv";
+import dbConnect from "./config/mongoose.js";
 
-export default connect;
+dbConnect();
---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+// connect to mongo with mongoose
+const mongoUri = `mongodb+srv://thisissamuelyeboah:${process.env.DB_USER_PASSWORD}@kwabscluster.d7jjk.mongodb.net/`;
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(mongoUri, { dbName: "subs" });
+    console.log("mongo db connection -- success");
+  } catch (error) {
+    console.log("mongo db connection -- error", error);
+  }
+};
+
+export default dbConnect;
