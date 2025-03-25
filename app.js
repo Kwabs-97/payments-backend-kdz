18a14528728a17173a880fe4a2adfb7f7bd11817
Samuel <khobewayne@gmail.com>
2025-03-25 03:47:25 +0000

    database connection success

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri = "mongodb+srv://thisissamuelyeboah:KhobeWayne@kwabscluster.d7jjk.mongodb.net/?retryWrites=true&w=majority&appName=KwabsCluster";
+import mongoose from "mongoose";
+const uri =
+  "mongodb+srv://thisissamuelyeboah:KhobeWayne@kwabscluster.d7jjk.mongodb.net/?retryWrites=true&w=majority&appName=KwabsCluster";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -8,7 +9,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -16,8 +17,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("subs").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -25,3 +28,15 @@ async function run() {
 }
 run().catch(console.dir);
 
+
+// connect to mongo with mongoose
+const connect = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://thisissamuelyeboah:KhobeWayne@kwabscluster.d7jjk.mongodb.net/", { dbName: 'subs' })
+    console.log("connection to subs database success from mongoose")
+  } catch (error) {
+    throw error("Error connecting to your database", error)
+  }
+}
+
+connect()
