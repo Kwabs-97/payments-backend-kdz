ddfbf1624785217f67ff05d9b8fbbc93619075f3
Samuel <khobewayne@gmail.com>
2025-03-26 15:17:57 +0000

    renamed data to constants, refactored gencsv out of main function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import dbConnect from "./config/mongoose.js";
 import { getSubs } from "./model/index.js";
-import { createObjectCsvWriter } from "csv-writer";
+import genCSV from "./utils/gen_csv.js";
 // dbConnect()
 
 export const recordsArr = []
@@ -15,38 +15,27 @@ async function main() {
     const data = Object.values(filteredSubscriptioins);
 
     const filteredFields = data.map((da) => {
-        const filteredFields = { business_id: da.business_id, email: da.email, plan_name: da.plan_id.name, plan_price: da.plan_id.price, payment_platform_name: da.payment_platform.name }
+        const filteredFields = { business_id: da.business_id, email: da.email, plan_id: da.plan_id._id, plan_name: da.plan_id.name, plan_price: da.plan_id.price, payment_platform_name: da.payment_platform.name }
         return filteredFields
     });
     recordsArr.push(filteredFields);
     recordsArr.flat();
+
+    const header = [
+        { id: 'business_id', title: 'business_id' },
+        { id: 'plan_id', title: 'plan_id' },
+        { id: 'email', title: 'email' },
+        { id: 'plan_name', title: 'plan_name' },
+        { id: 'plan_price', title: 'plan_price' },
+        { id: 'payment_platform_name', title: 'payment_platform_name' }
+    ]
+
+    await genCSV(header, recordsArr);
 }
 await main();
 
 
-const genCSV = async () => {
-    const csvWriter = createObjectCsvWriter({
-        path: 'file.csv',
-        header: [
-            { id: 'business_id', title: 'business_id' },
-            { id: 'email', title: 'email' },
-            { id: 'plan_name', title: 'plan_name' },
-            { id: 'plan_price', title: 'plan_price' },
-            { id: 'payment_platform_name', title: 'payment_platform_name' }
-        ]
-    });
 
-    // Use a single loop to process filteredHeaders
-    const records = filteredHeaders.map(header => ({
-        business_id: header.business_id,
-        email: header.email,
-        plan_name: header.plan_name,
-        plan_price: header.plan_price,
-        payment_platform_name: header.payment_platform_name
-    }));
-
-    await csvWriter.writeRecords(records);
-};
 
 await genCSV();
 
--- utils/gen_csv.js
+++ utils/gen_csv.js
@@ -1,28 +1,21 @@
 import { createObjectCsvWriter } from "csv-writer";
-async function genCSV(headers) {
-
+const genCSV = async (header ,recordsArr) => {
     const csvWriter = createObjectCsvWriter({
         path: 'file.csv',
-        header: [
-            { id: 'business_id', title: 'business_id' },
-            { id: 'email', title: 'email' },
-            // { id: 'plan_id', title: 'plan_id'},
-            { id: 'plan_name', title: 'plan_name' },
-            { id: 'plan_price', title: 'plan_price' },
-            { id: 'payment_platform_name', title: 'payment_platform_name' }
-        ]
+        header
     });
 
-    const records = [];
-    for (let i = 0; i < headers.length; i++) {
-        for (header in headers) {
-            records.push({
-                business_id: header.business_id, email: header.email, plan_name: header.plan.name, plan_price: header.plan_price, payment_platform_name: header.payment_platform_name
-            })
-        }
-    }
-    await csvWriter.writeRecords(records);
+    const records = recordsArr.map(record => ({
+        business_id: record.business_id,
+        email: record.email,
+        plan_id: record.plan_id,
+        plan_name: record.plan_name,
+        plan_price: record.plan_price,
+        payment_platform_name: record.payment_platform_name
+    }));
 
-}
+    await csvWriter.writeRecords(records);
+};
+
 
 export default genCSV;
